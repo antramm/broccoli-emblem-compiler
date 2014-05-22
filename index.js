d440adebd564caf9b06532cd0cb9ecbb7c817a1a
Gordon L. Hempton <ghempton@gmail.com>
2014-05-22 11:39:24 -0700

    added support for vanilla handlebars; turn off jshint

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var path = require('path');
 var Filter = require('broccoli-filter');
 var compiler = require('ember-template-compiler');
 var emblem = require('emblem');
+var Handlebars = require('handlebars');
 
 module.exports = TemplateCompiler
 TemplateCompiler.prototype = Object.create(Filter.prototype);
@@ -11,12 +12,21 @@ function TemplateCompiler (inputTree, options) {
         return new TemplateCompiler(inputTree, options);
     }
     this.inputTree = inputTree;
+    this.vanilla = options && options.vanilla || this.vanilla;
+    this.extensions = options && options.extensions || this.extensions;
+    this.targetExtension = options && options.targetExtension || this.targetExtension;
 }
 
+TemplateCompiler.prototype.vanilla = false;
 TemplateCompiler.prototype.extensions = ['embl', 'emblem'];
 TemplateCompiler.prototype.targetExtension = 'js';
 
 TemplateCompiler.prototype.processString = function (string) {
-  var compiled = emblem.precompile(compiler.EmberHandlebars, string);
-  return 'export default Ember.Handlebars.template(' + compiled + ');\n';
+  if(this.vanilla) {
+    var compiled = emblem.precompile(Handlebars, string);
+    return '/* jshint ignore:start */\nexport default Handlebars.template(' + compiled + ');\n/* jshint ignore:end */\n';
+  } else {
+    var compiled = emblem.precompile(compiler.EmberHandlebars, string);
+    return 'export default Ember.Handlebars.template(' + compiled + ');\n';
+  }
 }
